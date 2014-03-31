aa1525214a90bcdde321bf1e36eb57ee927c3740
Stian Eikeland <stian@eikeland.se>
2014-03-31 23:24:43 +0200

    indent

--- test/bløggr/core_test.clj
+++ test/bløggr/core_test.clj
@@ -3,9 +3,9 @@
   (:require [bløggr.core :refer :all]))
 
 (fact "get-assets loads assets and modifies path"
-      (get-assets) => (just [{:path "/images/image.jpg"} {:path "/fonts/font.wof"}] :in-any-order)
-      (provided
-       (load-assets "images") => [{:path "/image.jpg"}]
-       (load-assets "fonts") => [{:path "/font.wof"}]))
+  (get-assets) => (just [{:path "/images/image.jpg"} {:path "/fonts/font.wof"}] :in-any-order)
+  (provided
+    (load-assets "images") => [{:path "/image.jpg"}]
+    (load-assets "fonts") => [{:path "/font.wof"}]))
 
 
