fc4467e5877ae7e44719fb8c735bc2df060b0a55
Stian Eikeland <stian@eikeland.se>
2014-04-03 22:47:49 +0200

    bumped to clojure v 1.6.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://eikeland.se/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [stasis "1.0.0"]
                  [ring "1.2.2"]
                  [me.raynes/cegdown "0.1.1"]
