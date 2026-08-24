51e248dbe82846beaf3bee637d485b69d80edb5a
Stian Eikeland <stian@eike.land>
2026-08-24 19:59:08 +0200

    Add simple logger to quiet startup noise

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.4"]
+                 [org.slf4j/slf4j-simple "2.0.17"]
                  [stasis "2023.11.21"]
                  [ring "1.15.5"]
                  [ring/ring-codec "1.3.0"]
@@ -16,6 +17,7 @@
                  [org.clojure/data.xml "0.0.8"]]
   :aliases {"export" ["run" "-m" "bløggr.core/export"]
             "dev" ["run" "-m" "dev"]}
+  :jvm-opts ["-Dorg.slf4j.simpleLogger.defaultLogLevel=warn"]
   :profiles {:dev {:source-paths ["dev"]
                    :plugins [[lein-midje "3.2.2"]]
                    :dependencies [[midje "1.10.10" :exclusions [org.clojure/clojure]]]}})
