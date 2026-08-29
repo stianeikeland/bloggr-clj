d76818a85556652e9ddeac4881b27ef41bdedd39
Stian Eikeland <stian@eike.land>
2026-08-29 13:13:23 +0000

    Prevent mime magic from ruining css mime types on upload
    Seems to be a macos / brew issue?

--- sync.sh
+++ sync.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-s3cmd sync -P dist/ s3://blog.eikeland.se --cf-invalidate --cf-invalidate-default-index --add-header "Cache-Control: max-age=600"
+s3cmd sync -P dist/ s3://blog.eikeland.se \
+      --no-mime-magic \
+      --guess-mime-type \
+      --cf-invalidate \
+      --cf-invalidate-default-index \
+      --add-header "Cache-Control: max-age=600"
