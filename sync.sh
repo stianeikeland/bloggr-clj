6c83b5add11c3b2ec75741211f254a83e72dc1b5
Stian Eikeland <stian@eike.land>
2026-08-31 16:09:57 +0000

    Make sync not leak username etc

--- sync.sh
+++ sync.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 bin/generate-thumbs
 lein export
 
-s3cmd sync -P dist/ s3://blog.eikeland.se \
+s3cmd sync --no-preserve -P dist/ s3://blog.eikeland.se \
       --no-mime-magic \
       --guess-mime-type \
       --cf-invalidate \
